a4c3dafc876eeffcebd02b63c146d42f607d590a
Bahram Ismayilov <bahram.ismayilov@hotmail.com>
2019-12-20 17:44:07 +0400

    add - add to cart

--- js/app.js
+++ js/app.js
@@ -1,56 +1,79 @@
 (function() {
-  //all images
-  let imageList = [];
-  let counter = 0;
+    const cartInfo = document.getElementById('cart-info');
+    const cart = document.getElementById('cart');
 
-  const images = document.querySelectorAll('.store-img');
-  const container = document.querySelector('.lightbox-container');
-  const item = document.querySelector('.lightbox-item');
-  const closeIcon = document.querySelector('.lightbox-close');
-  const btnLeft = document.querySelector('.btnLeft');
-  const btnRight = document.querySelector('.btnRight');
-
-  //add all images to the array
-  images.forEach(function(img) {
-    imageList.push(img.src);
-  });
-  // console.log(imageList);
-  //open module
-  images.forEach(function(img) {
-    img.addEventListener('click', function(event){
-      container.classList.add('show');
-
-      //get source
-      let src = event.target.src;
-     // console.log(src);
-      counter = imageList.indexOf(src);
-      //console.log(counter);
-      //show modal with an image
-      item.style.backgroundImage = `url(${src})`;
-      
+    cartInfo.addEventListener('click', function() {
+        cart.classList.toggle('show-cart');
     });
-  });
-  // close module
-  closeIcon.addEventListener('click', function(){
-    container.classList.remove('show');
+})();
+
+//add items to the cart
+
+(function() {
+    const cartBtn = document.querySelectorAll('.store-item-icon');
+
+    cartBtn.forEach(function(btn) {
+        btn.addEventListener('click', function(event) {
+            //console.log(event.target);
+            
+            if(event.target.parentElement.classList.contains('store-item-icon')) {
+                let fullPath = event.target.parentElement.previousElementSibling.src;
+                let pos = fullPath.indexOf('img') + 3;
+                //console.log(path);
+                let partPath = fullPath.slice(pos);
+                
+                const item = {};
+                item.img = `img-cart${partPath}`;
+                let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+                item.name = name;
+
+                let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+                let finalPrivce = price.slice(1).trim();
+                price = finalPrivce;
+                item.price = price;
+                
+                const cartItem = document.createElement('div');
+                cartItem.classList.add('cart-item', 'd-flex', 'justify-content-between', 'text-capitalize', 'my-3');
+                cartItem.innerHTML = `
+                <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+                <div class="item-text">
     
-  });
+                  <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                  <span>$</span>
+                  <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+                </div>
+                <a href="#" id='cart-item-remove' class="cart-item-remove">
+                  <i class="fas fa-trash"></i>
+                </a>
+                `;
+                //select cart
+                const cart = document.getElementById('cart');
+                const total = document.querySelector('.cart-total-container');
 
-  btnLeft.addEventListener('click', function() {
-    counter--;
-    if(counter < 0) {
-      counter = imageList.length - 1;
-    }
-    item.style.backgroundImage = `url(${imageList[counter]})`;
-    console.log(counter);
-  });
-  btnRight.addEventListener('click', function() {
-    counter++;
-    if(counter > imageList.length - 1) {
-      counter = 0;
-    }
-    item.style.backgroundImage = `url(${imageList[counter]})`;
-    console.log(counter);
-  });
+                cart.insertBefore(cartItem, total);
+                alert('item added to the cart');
+                showTotal();
+            }
+        });
+    });
 
+    //show totals
+    function showTotal() {
+        const total = [];
+        const items = document.querySelectorAll('.cart-item-price');
+        items.forEach(function(item) {
+            total.push(parseFloat(item.textContent));
+        });
+        //console.log(total);
+
+        const totalMoney = total.reduce(function(total, item) {
+            total += item;
+            return total;
+        }, 0);
+        const finalMoney = totalMoney.toFixed(2);
+
+        document.getElementById('cart-total').textContent = finalMoney;
+        document.getElementById('item-count').textContent = total.length;
+        document.querySelector('.item-total').textContent = finalMoney;
+    }
 })();
